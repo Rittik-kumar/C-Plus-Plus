0b63857b42e18d52608db360d4d3a9f3d780dfdb
Rittik Kumar <rkrittik17@gmail.com>
2021-05-30 20:11:46 +0530

    basic i/o

--- Basics/IO.cpp
+++ Basics/IO.cpp
@@ -3,5 +3,22 @@ using namespace std;
 
 int main()
 {
+   cout<<"Hello";
+   cout<<"\n";
+   cout<<sqrt(16);
+   cout<<"\nHello World"<< " "<<"My name is Rittik";
+
+   int a=2;
+   int b=3;
+   int sum= a+b;
+   cout<<"\n"<<sum<<endl;
+
+   //char, int, float, double, bool
+   char c='a';
+   int d=3;
+   float e=5.5;
+   double f=3.5;
+   bool g=true;
+   cout<<c <<" " <<d <<" " <<e <<" " <<f  <<" " <<g << endl;
    
 }
