d089c1a3f5c19288964320e8826f287d8b9fac8d
Rittik Kumar <rkrittik17@gmail.com>
2021-05-30 20:12:10 +0530

    basic operations

---
+++ Basics/operations.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{  //  +,-,*,/,%
+   cout<<4+2 << endl;
+   cout<<4-2 << endl; 
+   cout<<4*2 << endl;
+   cout<<4/2 << endl;
+   cout<<4%2 << endl;
+   cout<<"\n";
+
+   //  ++, --
+   int a= 5;
+   a= a+1;
+   cout<<a << endl;
+   cout<<++a << endl;
+   cout<<a++ << endl;
+   cout<<a << endl;
+
+   cout<<"\n";
+
+   cout<<a << endl;
+   cout<<--a << endl;
+   cout<<a-- << endl;
+   cout<<a << endl;
+   
+}
