1db32dc394451294c4f2d76beadb16c597190048
Rittik Kumar <rkrittik17@gmail.com>
2021-06-01 13:15:51 +0530

    calculation order

---
+++ Basics/calculations.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ cout<< 7/2 << endl;
+  cout<< 7/2.0 << endl;
+  cout<< 'c'+1 << endl;
+
+  int a=3/2;
+  cout<<a << endl;
+
+  a=3/2.0;
+  cout<<a << endl;
+
+  double b=3/2.0;
+  cout<<b << endl;
+
+  b=3/2;
+  cout<<b << endl;
+}
