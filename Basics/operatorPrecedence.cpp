9f701a446646c367b8404c047638c02caf04ad95
Rittik Kumar <rkrittik17@gmail.com>
2021-06-01 13:16:20 +0530

    operator precedence

---
+++ Basics/operatorPrecedence.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ cout<<7/2*3 << endl;
+  cout<<3*7/2 << endl;
+
+  /* ++,--
+     !, unary +, unary -
+     *,/,%
+     +,-
+     <,<=,>,>=
+     ==,!=
+     &&
+     ||
+     ?:
+     =
+     ,
+  */
+}
