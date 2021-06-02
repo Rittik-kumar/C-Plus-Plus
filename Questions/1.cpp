0ec15f92cc0227893c623fd84b2e563efea25c0b
Rittik Kumar <rkrittik17@gmail.com>
2021-06-02 21:36:01 +0530

    Question 1

---
+++ Questions/1.cpp
@@ -0,0 +1,28 @@
+/*
+Given rectangle of length l, breadth b. Print area of rectangle.
+
+Constraints:
+1<= l <= 10^9
+1<= b <= 10^9
+
+Input format:
+2 space separated integer l and b
+
+Output format:
+Single number which is area of rectangle
+
+Sample input:
+3 4
+
+Sample output:
+13
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ long long int l, b;
+  cin>>l >>b;
+  cout<<l*b;
+}
