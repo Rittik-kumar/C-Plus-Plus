44976bf8880067cb65fb157f86e07603fbc8cc7e
Rittik Kumar <rkrittik17@gmail.com>
2021-06-02 21:37:07 +0530

    loops

---
+++ Basics/loops.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{   // while loop
+    int i =1;
+    while(i<=10)
+    {
+        cout<< i <<endl;
+        i+=1;
+    }
+
+    // for loop
+    for(int i=1;i<=10;i++)
+    {
+        cout<< i <<endl;
+    }
+
+    // do-while loop
+    i=1;
+    do{
+        cout<< i <<endl;
+        i+=1;
+    }while(i<=10);
+}
