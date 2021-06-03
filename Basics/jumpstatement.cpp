8e4a6cfcbd8f488028c3b0703637903e7c384c78
Rittik Kumar <rkrittik17@gmail.com>
2021-06-03 21:29:08 +0530

    jump statements

---
+++ Basics/jumpstatement.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{  // break
+   for(int i=0;i<10;i++)
+   {
+       if(i==8)
+        break;
+       cout<< i << endl;
+   }
+
+   cout<<"\n";
+
+   // continue
+   for(int i=0;i<10;i++)
+   {
+       if(i==8)
+        continue;
+       cout<< i << endl;
+   }
+}
