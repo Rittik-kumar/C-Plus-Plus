c5baa13bda3fabdad6a57556f939ed37221e5b53
Rittik Kumar <rkrittik17@gmail.com>
2021-06-03 21:29:55 +0530

    jump statement

---
+++ Questions/3.cpp
@@ -0,0 +1,30 @@
+/*
+Jump Statement
+
+Input:
+1
+2
+88
+42
+99
+
+Output:
+1
+2
+88
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    while(true)
+    {
+        int x;
+        cin>> x;
+        if(x==42)
+            break;
+        cout<< x << endl;
+    }
+}
