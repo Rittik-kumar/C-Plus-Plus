f5c55e9972574c6e92fb254b0454c01ca1684f46
Rittik Kumar <rkrittik17@gmail.com>
2021-06-09 21:36:19 +0530

    sum of digits

---
+++ Questions/4.cpp
@@ -0,0 +1,26 @@
+/*
+ Find the sum of digits
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ int t;
+  cin>>t;
+  while(t--)
+  {
+    int n;
+    cin>>n;
+    int digit_sum=0;
+
+    while(n>0)
+    { int last_digit = n%10;
+      digit_sum += last_digit;
+      n = n/10;
+    }
+
+    cout<< digit_sum << endl;
+  }
+
+}
